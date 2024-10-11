7a2ca3203add51771f5dbb65198af3340562411f
RedBenHub <redha.benchetrat@me.com>
2024-10-11 16:47:57 -0700

    ok

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = {
   env: {
     APP_ID: process.env.APP_ID,
-    APP_SECRET: process.env.APP_SECRET
-
+    APP_SECRET: process.env.APP_SECRET,
   },
 }
